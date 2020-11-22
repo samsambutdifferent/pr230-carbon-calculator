3aff02f52107709649d6e2b9964a8ae860f7de14
samsambutdifferent <sam@solutions.to>
2020-11-22 15:47:50 +0000

    update co2 index table component

--- co2_index_table.py
+++ co2_index_table.py
@@ -1,35 +1,38 @@
-import pandas as pd
-import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-CSV_PATH=os.path.join('.','data/co2_index.csv')
-COLS_TO_USE=['name', 'co2_index_per_g']
-df = pd.read_csv(CSV_PATH,
-                 usecols=COLS_TO_USE)
+cred = credentials.Certificate('./key.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 
-def calculate_food_item_co2_index(ingredientList):
+def calculate_ingredients_co2_index(ingredientList):
     '''
         Sum together all co2 values to get total
     '''
-    totalSum=0
-    for key,value in ingredientList.items():
-        totalSum += calculate_ingredient_co2_index(value, key)
-    return totalSum
+    totalSum = 0
+    for item in ingredientList:
+        passed, value = calculate_ingredient_co2_index(item.get('name'), item.get('weight'))
+        if passed:
+            totalSum += value
+            continue
+        else:
+            return False, totalSum
+    return True, totalSum
         
 
 def calculate_ingredient_co2_index(name, weight):
     '''
         Get c02 per gram value from co2 index table
     '''
-    isName = df['name'] == name
-    filtered = df[isName]
-    co2Val = filtered['co2_index_per_g'].iloc[0]
-    print(co2Val * weight)
-    return co2Val * weight
-    
-def return_all_available_ingredients():
-    '''
-        return list of all currently available food items
-    '''
-    return pd.unique(df['name'])
-    
+    doc = db.collection(u'carbon_index').document(name).get()
+
+    if doc.exists:
+        doc_data = doc.to_dict()
+        co2_index_per_g = doc_data.get('co2_index_per_g')
+        co2ValperGram = co2_index_per_g * weight
+        return True, co2ValperGram
+    else:
+        print(f'No such document: {name}')
+        return False, ''
